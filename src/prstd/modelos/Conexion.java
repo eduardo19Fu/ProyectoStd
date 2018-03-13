0031b2a967ced987cfff6fdf0fcd6bfa5f487350
eduardo19Fu <ramieduar@gmail.com>
2018-03-12 22:06:03 -0600

    Segundo Commit version 0.1

--- src/prstd/modelos/Conexion.java
+++ src/prstd/modelos/Conexion.java
@@ -1,9 +1,78 @@
 package prstd.modelos;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Edfu-Pro
  */
 public class Conexion {
+    
+    private Connection connection;
+    private String db;
+    private String user;
+    private String pass;
+    private String url;
+    private String driver;
+
+    public Conexion() {
+    }
+
+    public Conexion(Connection connection, String db, String user, String pass, String url, String driver) {
+        this.connection = connection;
+        this.db = db;
+        this.user = user;
+        this.pass = pass;
+        this.url = url;
+        this.driver = driver;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public String getDb() {
+        return db;
+    }
+
+    public void setDb(String db) {
+        this.db = db;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
     
 }
